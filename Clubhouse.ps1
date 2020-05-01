358f93ec8fb3a4757be2797e207bd689a4670b2d
Kieran Chandler <bwoodyouth_14@hotmail.co.uk>
2020-05-01 20:07:04 +0100

    Moved clubhouse story creation into Clubhouse.ps1

--- Clubhouse.ps1
+++ Clubhouse.ps1
@@ -1,3 +1,11 @@
-function CreateStory() {
-    
+function New-Story(
+    [string] $Name,
+    [string] $Created,
+    [string] $ProjectId
+) {
+    [PSCustomObject]@{
+        name       = $Name
+        project_id = $ProjectId
+        created_at = $Created
+    } | ConvertTo-Json
 }
--- Trello-To-Clubhouse.ps1
+++ Trello-To-Clubhouse.ps1
@@ -6,22 +6,22 @@ param (
 )
 
 . "./Trello.ps1"
+. "./Clubhouse.ps1"
 
 $trelloSrcObj = ($TrelloSrcJson | ConvertFrom-Json)
 
 function ConvertTo-ClubHouseStory([psobject]$trelloCard) {
     $action = (GetCardCreatedAction -cardId $trelloCard.id -allActions $trelloSrcObj.actions)
 
-    [PSCustomObject]@{
-        name       = $trelloCard.name
-        project_id = $ClubhouseProjectId
-        created_at = $action.date
-    }
+    New-Story `
+        -Name $trelloCard.name `
+        -ProjectId $ClubhouseProjectId `
+        -Created $action.date
 }
 
 foreach ($card in $trelloSrcObj.Cards) {
     $isNotAnArchivedCard = -not $card.closed
     if ($isNotAnArchivedCard) {
-        ConvertTo-ClubHouseStory $card | ConvertTo-Json
+        ConvertTo-ClubHouseStory $card
     }
 }
