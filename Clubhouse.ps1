7e8078946c2f9a1de18c81c2b0235d87b7c6e5af
Kieran Chandler <bwoodyouth_14@hotmail.co.uk>
2020-05-05 01:38:05 +0100

    Implemented inital migration of epics

--- Clubhouse.ps1
+++ Clubhouse.ps1
@@ -57,3 +57,56 @@ function New-Story(
         -UseBasicParsing
     Write-Host $(SimplifyWebResponse -WebResponse $response)
 }
+
+function New-Epic(
+    [string] $ApiToken,
+    [string] $Name,
+    [string] $Description,
+    [string] $Created,
+    [string] $Updated,
+    [string[]] $OwnerIds,
+    [string[]] $LabelNames,
+    [int[]] $AttachmentIds
+) {
+    if ($null -eq $LabelNames) {
+        $LabelNames = @()
+    }
+    else {
+        [PSCustomObject[]]$labels = $LabelNames | Select-Object `
+        @{
+            N = "name";
+            E = { $_ }
+        }
+    }
+
+    if ([string]::IsNullOrWhiteSpace($Created)) {
+        $Created = "0001-01-01T00:00:00.000Z"
+    }
+
+    if ([string]::IsNullOrWhiteSpace($Updated)) {
+        $Updated = "0001-01-01T00:00:00.000Z"
+    }
+
+    if ($null -eq $OwnerIds) {
+        $OwnerIds = @()
+    }
+
+    $requestBody = [PSCustomObject]@{
+        name       = $Name
+        description = $Description
+        created_at = $Created
+        updated_at = $Updated
+        owner_ids = $OwnerIds
+        labels  = $labels
+    } | ConvertTo-Json
+
+    Write-Host
+    Write-Host "Creating epic via Clubhouse API.."
+    $response = Invoke-WebRequest `
+        -Uri "https://api.clubhouse.io/api/v3/epics?token=$ApiToken" `
+        -Method "POST" `
+        -ContentType "application/json" `
+        -Body $requestBody `
+        -UseBasicParsing
+    Write-Host $(SimplifyWebResponse -WebResponse $response)
+}
--- Trello-To-Clubhouse.ps1
+++ Trello-To-Clubhouse.ps1
@@ -50,9 +50,46 @@ function ConvertTo-ClubHouseStory([string]$ApiToken, [psobject]$trelloCard, $Use
         -LabelNames $labelNames
 }
 
+function ConvertTo-ClubHouseEpic([string]$ApiToken, [psobject]$trelloCard, $UsersMap) {
+    $action = (GetCardCreatedAction -cardId $trelloCard.id -allActions $trelloSrcObj.actions)
+
+    $ownerIds = $trelloCard.idMembers `
+    | Select-Object `
+    @{
+        N = "ClubhouseUserId";
+        E = { FromTrelloUserIdToClubhouseUserId -TrelloUserId $_ -Users $TrelloToClubhouseUserMap }
+    } `
+    | Select-Object -ExpandProperty ClubhouseUserId `
+    | Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+
+    $labelNames = $trelloCard.labels | Select-Object -ExpandProperty name
+
+    New-Epic `
+        -ApiToken $ApiToken `
+        -Name $trelloCard.name `
+        -Description $trelloCard.desc`
+        -Created $action.date `
+        -Updated $trelloCard.dateLastActivity `
+        -OwnerIds $ownerIds `
+        -LabelNames $labelNames
+}
+
+$epicListId = "5e0010e3d50d53272ede97d4"
 foreach ($card in $trelloSrcObj.Cards) {
     $isNotAnArchivedCard = -not $card.closed
-    $isNotAMilestoneCard = $card.idList -ne "5e0010e3d50d53272ede97d4"
+    $isNotAMilestoneCard = $card.idList -ne $epicListId
+    $isAMilestoneCard = $card.idList -eq $epicListId
+
+    if ($isNotAnArchivedCard -and $isAMilestoneCard) {
+        ConvertTo-ClubHouseEpic $ClubhouseApiToken $card $TrelloToClubhouseUserMap
+    }
+}
+
+foreach ($card in $trelloSrcObj.Cards) {
+    $isNotAnArchivedCard = -not $card.closed
+    $isNotAMilestoneCard = $card.idList -ne $epicListId
+    $isAMilestoneCard = $card.idList -eq $epicListId
+
     if ($isNotAnArchivedCard -and $isNotAMilestoneCard) {
         ConvertTo-ClubHouseStory $ClubhouseApiToken $card $TrelloToClubhouseUserMap $WorkflowStateMap
     }
